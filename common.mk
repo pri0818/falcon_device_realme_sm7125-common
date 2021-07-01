e72f2c80b5114053d5c7572aa21f09d112d47f9c
Alexander Winkowski <dereference23@outlook.com>
2021-07-01 09:51:47 +0200

    sm7125-common: Build power AIDL HAL
    Change-Id: I0e10f0b36598b8cbf25ee2dc08b8a4eced95c2ed
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- common.mk
+++ common.mk
@@ -282,7 +282,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service
+    android.hardware.power-service.xiaomi-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # QTI FWK Detect
 PRODUCT_PACKAGES += \

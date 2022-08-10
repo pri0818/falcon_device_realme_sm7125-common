9281fa84fe84d7bc4a40d736aa69ea87522ba633
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-10 06:43:46 +0000

    sm7125-common: power: Add custom mode extension for handling app launch
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the first app launch hinting through
    device-specific mode override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside app launch (V1)
    config inside perfboostsconfig.xml
    
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,6 +189,7 @@ TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # Power
 TARGET_POWERHAL_BOOST_EXT := $(COMMON_PATH)/power/boost-ext.cpp
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/mode-ext.cpp
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true

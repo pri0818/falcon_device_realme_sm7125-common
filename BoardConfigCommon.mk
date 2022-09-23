5e9494be9cbf39046e210cfd4dd98b9bab58d1c3
Edwin Moquete <edwinmmoquete@gmail.com>
2022-09-23 05:59:42 +0000

    sm7125-common: Switch to legacy sepolicy_vndr
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,7 +212,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
